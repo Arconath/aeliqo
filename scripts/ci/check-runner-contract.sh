8bccbd229f272a4e54fdda689be9cf1828e3ba69
Hermawan Nino <hermawan.remote@gmail.com>
2026-09-07 13:55:55 +0700

    Use containerized BuildKit for image provenance

--- scripts/ci/check-runner-contract.sh
+++ scripts/ci/check-runner-contract.sh
@@ -6,6 +6,7 @@ case "${1:-static}" in
     test "$(id -u)" -ne 0
     docker version >/dev/null
     docker buildx version >/dev/null
+    docker buildx inspect "${BUILDX_BUILDER:-default}" >/dev/null
     ;;
   static)
     test "$(find .github/workflows -maxdepth 1 -type f -name '*.yml' -print | wc -l | tr -d ' ')" = 3
